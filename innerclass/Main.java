f6cc07129d851e9319a4474b2916f85053b67f08
briankaboyi <brianmkaboyi@gmail.com>
2023-12-20 12:15:32 +0300

    explored private and static inner class

--- innerclass/Main.java
+++ innerclass/Main.java
@@ -6,16 +6,21 @@ public class Main {
     public static void main (String args[]){
         OuterClass outerclass = new OuterClass();
         OuterClass.InnerClass innerclass = outerclass.new InnerClass();
+        // OuterClass.InnerClass innerclass = new OuterClass.InnerClass();
         innerclass.sum(outerclass.x,innerclass.y);
+        System.out.println(outerclass.x+innerclass.y);
     }
    
 }
+//An inner class can also be static, which means that you can access it without creating an object of the outer class.
+
+//innerclasses that are not static have access to attributes and methods of the outerclass.
 class OuterClass{
     int x =10;
     class InnerClass{
         int y = 5;
         public void sum(int a,int b){
-            System.out.println(a+b+x+y);
+            System.out.println(a+b+y);
         }
     } 
 }
